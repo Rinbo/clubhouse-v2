5d7fcc2652d6b8605c5e71331b83f459a834c9d3
Rinbo <robin.b@outlook.com>
2020-07-30 19:56:01 +0200

    Add integration tests to test user registration with children

--- src/test/java/nu/borjessons/clubhouse/RegistrationEndpointTests.java
+++ src/test/java/nu/borjessons/clubhouse/RegistrationEndpointTests.java
@@ -3,19 +3,31 @@ package nu.borjessons.clubhouse;
 import static io.restassured.RestAssured.given;
 import static nu.borjessons.clubhouse.integration.util.RequestModels.ADMIN_USER_NAME;
 import static nu.borjessons.clubhouse.integration.util.RequestModels.ADMIN_USER_USERNAME;
-import static nu.borjessons.clubhouse.integration.util.RequestModels.CLUB1;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.CHILD_1_NAME;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.CHILD_2_NAME;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.CLUB_1;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.CLUB_2;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.NORMAL_USER1_NAME;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.NORMAL_USER_USERNAME;
 import static nu.borjessons.clubhouse.integration.util.RequestModels.clubRegistrationRequest;
 import static nu.borjessons.clubhouse.integration.util.RequestModels.loginRequest;
+import static nu.borjessons.clubhouse.integration.util.RequestModels.userWithChildrenRegistrationRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
-
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -25,7 +37,7 @@ import nu.borjessons.clubhouse.config.TestConfiguration;
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 class RegistrationEndpointTests {
 	
-	private static String clubId;
+	private static Long clubId;
 	private static String authToken;
 	
 	@LocalServerPort
@@ -42,7 +54,7 @@ class RegistrationEndpointTests {
 	void aa_registerClub() {
 		Response response = given().contentType(TestConfiguration.APPLICATION_JSON)
 				.accept(TestConfiguration.APPLICATION_JSON)
-				.body(clubRegistrationRequest(CLUB1, ADMIN_USER_USERNAME, ADMIN_USER_NAME))
+				.body(clubRegistrationRequest(CLUB_1, ADMIN_USER_USERNAME, ADMIN_USER_NAME))
 				.when()
 				.post("/register/club")
 				.then()
@@ -51,9 +63,66 @@ class RegistrationEndpointTests {
 				.extract()
 				.response();
 
-		clubId = response.jsonPath().getString("activeClub");
+		clubId = Long.valueOf(response.jsonPath().getString("activeClub"));
 		assertNotNull(clubId);
 	}
+	
+	@Test
+	void ab_conflictWhenRegisterSameClub() {
+		Response response = given().contentType(TestConfiguration.APPLICATION_JSON)
+				.accept(TestConfiguration.APPLICATION_JSON)
+				.body(clubRegistrationRequest(CLUB_1, ADMIN_USER_USERNAME, ADMIN_USER_NAME))
+				.when()
+				.post("/register/club")
+				.then()
+				.statusCode(409)
+				.contentType(TestConfiguration.APPLICATION_JSON).extract()
+				.response();
+		
+		String errorResponse = response.jsonPath().getString("message");
+		assertThat(errorResponse).contains("A resource already exists in the database with provided parameters");
+	}
+	
+	@Test
+	void ac_conflictWhenRegisterDifferentClubButSameUser() {
+		Response response = given().contentType(TestConfiguration.APPLICATION_JSON)
+				.accept(TestConfiguration.APPLICATION_JSON)
+				.body(clubRegistrationRequest(CLUB_2, ADMIN_USER_USERNAME, ADMIN_USER_NAME))
+				.when()
+				.post("/register/club")
+				.then()
+				.statusCode(409)
+				.contentType(TestConfiguration.APPLICATION_JSON).extract()
+				.response();
+		
+		String errorResponse = response.jsonPath().getString("message");
+		assertThat(errorResponse).contains("A resource already exists in the database with provided parameters");
+	}
+	
+	@Test
+	void ba_registerUserWithChildren() {
+		Response response = given().contentType(TestConfiguration.APPLICATION_JSON)
+				.accept(TestConfiguration.APPLICATION_JSON)
+				.body(userWithChildrenRegistrationRequest(clubId, NORMAL_USER_USERNAME, NORMAL_USER1_NAME, new ArrayList<>(Arrays.asList(CHILD_1_NAME, CHILD_2_NAME))))
+				.when()
+				.post("/register/user")
+				.then()
+				.statusCode(200)
+				.contentType(TestConfiguration.APPLICATION_JSON)
+				.extract()
+				.response();
+
+		String email = response.jsonPath().getString("email");
+		List<String> roles = response.jsonPath().getList("roles", String.class);
+		List<Long> childrenIds  = response.jsonPath().getList("childrenIds", Long.class);
+		assertTrue(roles.contains("PARENT"));
+		assertTrue(roles.contains("USER"));
+		assertEquals(2, childrenIds.size());
+		assertNotNull(email);
+		assertEquals(NORMAL_USER_USERNAME, email);
+	}
+
+	
 	
 	@Test
 	void ba_loginUser() {
--- src/test/java/nu/borjessons/clubhouse/integration/util/RequestModels.java
+++ src/test/java/nu/borjessons/clubhouse/integration/util/RequestModels.java
@@ -1,7 +1,9 @@
 package nu.borjessons.clubhouse.integration.util;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RequestModels {
 	
@@ -13,13 +15,16 @@ public class RequestModels {
 	
 	public static final String GENERIC_PASSWORD = "1234567890";
 	
-	public static final String CLUB1 = "Judo BK";
+	public static final String CLUB_1 = "Judo BK";
+	public static final String CLUB_2 = "Borjessons BK";
 	
 	public static final String ADMIN_USER_USERNAME = "admin@outlook.com";
 	public static final String[] ADMIN_USER_NAME = new String[] {"Admin", "Adminsson" };
 	public static final String NORMAL_USER_USERNAME = "user@outlook.com";
 	public static final String[] NORMAL_USER1_NAME = new String[] {"User", "Usersson" };
 	public static final String[] NORMAL_USER1_CHILDS_NAME = new String[] {"Child", "Childsson" };
+	public static final String CHILD_1_NAME = "Sixten Childsson";
+	public static final String CHILD_2_NAME = "Albin Childsson";
 
 	public static Map<String, Object> clubRegistrationRequest(String ClubName, String owner, String[] name) {
 		Map<String, Object> organizationRequestModel = new HashMap<>();
@@ -32,7 +37,7 @@ public class RequestModels {
 		userRequestModel.put("firstName", name[0]);
 		userRequestModel.put("lastName", name[1]);
 		userRequestModel.put("password", GENERIC_PASSWORD);
-		userRequestModel.put("clubId", "dummy");
+		userRequestModel.put("clubId", 0);
 
 		organizationRequestModel.put("owner", userRequestModel);
 
@@ -41,10 +46,10 @@ public class RequestModels {
 		return organizationRequestModel;
 	}
 
-	public static Map<String, Object> userRegistrationRequest(String clubId, String user, String[] name) {
+	public static Map<String, Object> userRegistrationRequest(Long clubId, String user, String[] name) {
 		Map<String, Object> userRequestModel = new HashMap<>();
 
-		userRequestModel.put("email", NORMAL_USER_USERNAME);
+		userRequestModel.put("email", user);
 		userRequestModel.put("firstName", name[0]);
 		userRequestModel.put("lastName", name[1]);
 		userRequestModel.put("password", GENERIC_PASSWORD);
@@ -54,16 +59,35 @@ public class RequestModels {
 
 		return userRequestModel;
 	}
-
-	public static Map<String, Object> childRegistrationRequest(String[] name) {
+	
+	public static Map<String, Object> userWithChildrenRegistrationRequest(Long clubId, String user, String[] name,
+			List<String> childrenNames) {
 		Map<String, Object> userRequestModel = new HashMap<>();
 
+		userRequestModel.put("email", user);
 		userRequestModel.put("firstName", name[0]);
 		userRequestModel.put("lastName", name[1]);
+		userRequestModel.put("password", GENERIC_PASSWORD);
+		userRequestModel.put("clubId", clubId);
+		
+		List<Map<String, Object>> children = childrenNames.stream().map(child -> childRegistrationRequest(child)).collect(Collectors.toList());
+		
+		userRequestModel.put("children", children);
+		
+		return userRequestModel;
+	}
+
+	public static Map<String, Object> childRegistrationRequest(String name) {
+		Map<String, Object> childRequestModel = new HashMap<>();
+		
+		String[] nameArr = name.split(" ");
+
+		childRequestModel.put("firstName", nameArr[0]);
+		childRequestModel.put("lastName", nameArr[1]);
 		
 		userCounter++;
 
-		return userRequestModel;
+		return childRequestModel;
 	}
 
 	public static Map<String, Object> loginRequest(String username) {
