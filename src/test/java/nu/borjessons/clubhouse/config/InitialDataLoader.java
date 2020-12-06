40167db650fe35155d66651ab0d46d01b74d5c38
Rinbo <robin.b@outlook.com>
2020-12-06 18:45:09 +0100

    Add integration test for getting all clubs from public endpoint

--- src/test/java/nu/borjessons/clubhouse/config/InitialDataLoader.java
+++ src/test/java/nu/borjessons/clubhouse/config/InitialDataLoader.java
@@ -1,46 +1,41 @@
 package nu.borjessons.clubhouse.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
+import lombok.RequiredArgsConstructor;
 import nu.borjessons.clubhouse.controller.model.request.CreateClubModel;
 import nu.borjessons.clubhouse.controller.model.request.CreateUserModel;
 import nu.borjessons.clubhouse.data.Club.Type;
 import nu.borjessons.clubhouse.dto.UserDTO;
-import nu.borjessons.clubhouse.repository.ClubRepository;
 import nu.borjessons.clubhouse.service.RegistrationService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
+@RequiredArgsConstructor
 @Profile("test")
 public class InitialDataLoader {
 
-	private final RegistrationService registrationService;
-	
-	@Autowired
-	public InitialDataLoader(RegistrationService registrationService, ClubRepository clubRepository) {
-		this.registrationService = registrationService;
-		loadData();
-	}
-	
-	private void loadData() {
-		
-		CreateUserModel owner = new CreateUserModel();
-		owner.setFirstName("Alle");
-		owner.setLastName("Allesson");
-		owner.setDateOfBirth("1980-01-01");
-		owner.setClubId("dummy");
-		owner.setEmail("alle@clubhouse.com");
-		owner.setPassword("1234567890");
-		
-		CreateClubModel clubModel = new CreateClubModel();
-		clubModel.setName("Alles IK");
-		clubModel.setType(Type.SPORT);
-		clubModel.setOwner(owner);
-		
-		UserDTO dto = registrationService.registerClub(clubModel);
-		
-		System.out.println(dto.getClubId());
+  private final RegistrationService registrationService;
 
-	}
+  @PostConstruct
+  private void loadData() {
+
+    CreateUserModel owner = new CreateUserModel();
+    owner.setFirstName("Alle");
+    owner.setLastName("Allesson");
+    owner.setDateOfBirth("1980-01-01");
+    owner.setClubId("dummy");
+    owner.setEmail("alle@clubhouse.com");
+    owner.setPassword("1234567890");
+
+    CreateClubModel clubModel = new CreateClubModel();
+    clubModel.setName("Alles IK");
+    clubModel.setType(Type.SPORT);
+    clubModel.setOwner(owner);
+
+    UserDTO dto = registrationService.registerClub(clubModel);
+
+    System.out.println(dto.getClubId());
+  }
 }
