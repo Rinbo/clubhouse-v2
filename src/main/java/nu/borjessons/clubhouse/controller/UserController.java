bf3e1905fd95c4081b39b7f61ef1afde346cbb55
Rinbo <robin.b@outlook.com>
2020-12-25 21:27:45 +0100

    Fix bug where a user can could use join-club to join an already joined club

--- src/main/java/nu/borjessons/clubhouse/controller/UserController.java
+++ src/main/java/nu/borjessons/clubhouse/controller/UserController.java
@@ -1,118 +1,119 @@
 package nu.borjessons.clubhouse.controller;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import lombok.RequiredArgsConstructor;
 import nu.borjessons.clubhouse.controller.model.request.UpdateUserModel;
 import nu.borjessons.clubhouse.data.Club;
 import nu.borjessons.clubhouse.data.User;
 import nu.borjessons.clubhouse.dto.UserDTO;
 import nu.borjessons.clubhouse.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @RestController
 public class UserController extends AbstractController {
-	
-	private final UserService userService;
-	
-	/*
-	 * Principal routes
-	 */
-	
-	@PreAuthorize("hasRole('USER')")
-	@GetMapping("/principal")
-	public UserDTO getSelf() {
-		User user = getPrincipal();
-		return new UserDTO(user, user.getActiveClubId());
-	}
-	
-	@PreAuthorize("hasRole('USER')")
-	@PutMapping("/principal")
-	public UserDTO updateSelf(@RequestBody UpdateUserModel userDetails) {
-		User user = getPrincipal();
-		return userService.updateUser(user, user.getActiveClubId(), userDetails);
-	}
-	
-	@DeleteMapping("/principal")
-	public void deleteSelf() {
-		User user = getPrincipal();
-		userService.deleteUser(user);
-	}
-	
-	@PreAuthorize("hasRole('USER')")
-	@PutMapping("/principal/leave-club")
-	public void leaveClub() {
-		User user = getPrincipal();
-		userService.removeUserFromClub(user, user.getActiveClub());
-	}
-	
-	@PutMapping("/principal/switch-club")
-	public UserDTO switchClub(@RequestParam String clubId) {
-		User user = getPrincipal();
-		Optional<Club> maybeClub = user.getClubs().stream().filter(club -> club.getClubId().equals(clubId)).findFirst();
-		if (maybeClub.isEmpty()) throw new ResponseStatusException(HttpStatus.FORBIDDEN, String.format("User with id %s does not have access to club with id %s", user.getUserId(), clubId));
-		return userService.switchClub(user, maybeClub.get());
-	}
-	
-	@PutMapping("/principal/join-club")
-	public UserDTO joinClub(@RequestParam String clubId) {
-		User user = getPrincipal();
-		return userService.joinClub(user, clubId);
-	}
-	
-	/*
-	 * Administrator routes
-	 */
-	
-	@PreAuthorize("hasRole('ADMIN')")
-	@GetMapping("/{userId}")
-	public UserDTO getUser(@PathVariable String userId) {
-		Club club = getPrincipal().getActiveClub();
-		return new UserDTO(club.getUser(userId), club.getClubId());
-	}
-	
-	@PreAuthorize("hasRole('ADMIN')")
-	@PutMapping("/{userId}")
-	public UserDTO updateUser(@PathVariable String userId, @RequestBody UpdateUserModel userDetails) {
-		Club club = getPrincipal().getActiveClub();
-		return userService.updateUser(club.getUser(userId), club.getClubId(), userDetails);
-	}
-	
-	// Principal needs similar functionality to add another parent to his/her children. Needs helper
-	// end point to search for a user in the club by email
-	@PreAuthorize("hasRole('ADMIN')")
-	@PostMapping("/children/{userId}")
-	public UserDTO updateUserChildren(@PathVariable String userId, @RequestBody Set<String> childrenIds) {
-		Club club = getPrincipal().getActiveClub();
-		User parent = club.getUser(userId);
-		Set<User> clubChildren = club.getManagedUsers();
-		Set<User> validatedChildren = clubChildren.stream().filter(child -> childrenIds.contains(child.getUserId())).collect(Collectors.toSet());
-		return userService.updateUserChildren(parent, validatedChildren, club);
-	}
-	
-	@PreAuthorize("hasRole('ADMIN')")
-	@PutMapping("/remove/{userId}")
-	public void removeUserFromClub(@PathVariable String userId) {
-		Club club = getPrincipal().getActiveClub();
-		User user = club.getUser(userId);
-		userService.removeUserFromClub(user, club);
-	}
-	
-	
+
+  private final UserService userService;
+
+  /*
+   * Principal routes
+   */
+
+  @PreAuthorize("hasRole('USER')")
+  @GetMapping("/principal")
+  public UserDTO getSelf() {
+    User user = getPrincipal();
+    return new UserDTO(user, user.getActiveClubId());
+  }
+
+  @PreAuthorize("hasRole('USER')")
+  @PutMapping("/principal")
+  public UserDTO updateSelf(@RequestBody UpdateUserModel userDetails) {
+    User user = getPrincipal();
+    return userService.updateUser(user, user.getActiveClubId(), userDetails);
+  }
+
+  @DeleteMapping("/principal")
+  public void deleteSelf() {
+    User user = getPrincipal();
+    userService.deleteUser(user);
+  }
+
+  @PreAuthorize("hasRole('USER')")
+  @PutMapping("/principal/leave-club")
+  public void leaveClub() {
+    User user = getPrincipal();
+    userService.removeUserFromClub(user, user.getActiveClub());
+  }
+
+  @PutMapping("/principal/switch-club")
+  public UserDTO switchClub(@RequestParam String clubId) {
+    User user = getPrincipal();
+    Optional<Club> maybeClub =
+        user.getClubs().stream().filter(club -> club.getClubId().equals(clubId)).findFirst();
+    if (maybeClub.isEmpty())
+      throw new ResponseStatusException(
+          HttpStatus.FORBIDDEN,
+          String.format(
+              "User with id %s does not have access to club with id %s", user.getUserId(), clubId));
+    return userService.switchClub(user, maybeClub.get());
+  }
+
+  @PutMapping("/principal/join-club")
+  public UserDTO joinClub(@RequestParam String clubId) {
+    User user = getPrincipal();
+    if (user.getClubs().stream().anyMatch(club -> club.getClubId().equals(clubId)))
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, "User is already a member of this club");
+    return userService.joinClub(user, clubId);
+  }
+
+  /*
+   * Administrator routes
+   */
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @GetMapping("/{userId}")
+  public UserDTO getUser(@PathVariable String userId) {
+    Club club = getPrincipal().getActiveClub();
+    return new UserDTO(club.getUser(userId), club.getClubId());
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @PutMapping("/{userId}")
+  public UserDTO updateUser(@PathVariable String userId, @RequestBody UpdateUserModel userDetails) {
+    Club club = getPrincipal().getActiveClub();
+    return userService.updateUser(club.getUser(userId), club.getClubId(), userDetails);
+  }
+
+  // Principal needs similar functionality to add another parent to his/her children. Needs helper
+  // end point to search for a user in the club by email
+  @PreAuthorize("hasRole('ADMIN')")
+  @PostMapping("/children/{userId}")
+  public UserDTO updateUserChildren(
+      @PathVariable String userId, @RequestBody Set<String> childrenIds) {
+    Club club = getPrincipal().getActiveClub();
+    User parent = club.getUser(userId);
+    Set<User> clubChildren = club.getManagedUsers();
+    Set<User> validatedChildren =
+        clubChildren.stream()
+            .filter(child -> childrenIds.contains(child.getUserId()))
+            .collect(Collectors.toSet());
+    return userService.updateUserChildren(parent, validatedChildren, club);
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @PutMapping("/remove/{userId}")
+  public void removeUserFromClub(@PathVariable String userId) {
+    Club club = getPrincipal().getActiveClub();
+    User user = club.getUser(userId);
+    userService.removeUserFromClub(user, club);
+  }
 }
